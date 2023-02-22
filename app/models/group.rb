6d5779bba54b51cd7772578af6466d003489bad2
Goruchie <marcosgualtero43253@gmail.com>
2023-02-22 17:31:25 -0300

    Add valitarions to models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,14 @@
 class Group < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   has_many :entities
+
+  validates :name, length: { maximum: 80 }, presence: true
+
+  def self.total_amount(group)
+    total_amount = 0
+    group.entities.each do |entity|
+      total_amount += entity.amount
+    end
+    total_amount
+  end
 end
