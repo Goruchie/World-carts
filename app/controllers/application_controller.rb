f0b7b312c558c02e98f1348e931c7eb43a194f27
Goruchie <marcosgualtero43253@gmail.com>
2023-02-25 01:11:11 -0300

    Add styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(_resource)
+    groups_path(current_user)
+  end
+
   protected
 
   def authenticate_user!
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root "home#index"
+  
   devise_for :users
-  resources :groups
-  resources :users
-  resources :entities
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/', to: 'home#index', as: 'home'
+  root "groups#index"
+  resources :groups do
+    resources :entities
+  end
 end
